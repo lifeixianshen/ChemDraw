21a8ff1e4f262155aac21c7e51408ff4bedca6d6
Mehul09 <mehulsutariya09@gmail.com>
2019-07-29 23:04:38 +0530

    change it function

--- PFD.py
+++ PFD.py
@@ -24,10 +24,9 @@ class mywindow(QMainWindow):
         self.ui.pushButton_heater.clicked.connect(self.heater_main)
         self.ui.pushButton_cooler.clicked.connect(self.cooler_main)
         self.scene = QtWidgets.QGraphicsScene()
-        self.scene.setItemIndexMethod(QtWidgets.QGraphicsScene.BspTreeIndex)
+        self.scene.setItemIndexMethod(QtWidgets.QGraphicsScene.NoIndex)
         self.ui.pfdenv.setScene(self.scene)
-        self.ui.pfdenv.mousePressEvent = self.pfd_mousepress
-        self.ui.pfdenv.mouseMoveEvent = self.pfd_mousemove
+
         scroll_horizontal = self.ui.pfdenv.horizontalScrollBar()
         scroll_vertical = self.ui.pfdenv.verticalScrollBar()
         scroll_horizontal.valueChanged.connect(self.scroll_h)
@@ -35,7 +34,7 @@ class mywindow(QMainWindow):
 
         # menubar
         self.ui.actionAbout.triggered.connect(self.About)
-        self.ui.status_label_3.mousePressEvent=self.About_statusbar
+        self.ui.status_label_3.mousePressEvent = self.About_statusbar
 
     # defination for graphics view
     def scroll_v(self, V):
@@ -50,7 +49,6 @@ class mywindow(QMainWindow):
         global x, y
         x = e.x()
         y = e.y()
-        # print(str(H_pos)+str(V_pos))
         global UO
         global icon_select
         if icon_select == 1:
@@ -68,15 +66,18 @@ class mywindow(QMainWindow):
         if UO == "Cooler":
             add_icon = self.scene.addPixmap(QtGui.QPixmap("icons\Cooler.png"))
         add_icon.setPos(int(x + H_pos), int(y + V_pos))
-        add_icon.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable)
+        add_icon.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable, True)
+        add_icon.setFlag(QtWidgets.QGraphicsItem.ItemIsSelectable, True)
 
     def pfd_mousemove(self, e):
         x = e.x()
         y = e.y()
         self.ui.status_lable.setText(str("x:") + str(x) + "  " + "y:" + str(y))
 
-    # defination for icon menu
+    # definition for icon menu
     def heater_main(self):
+        self.ui.pfdenv.mousePressEvent = self.pfd_mousepress
+        self.ui.pfdenv.mouseMoveEvent = self.pfd_mousemove
         self.ui.log_pan.append(
             "[" + str(time.strftime("%H:%M:%S", time.localtime())) + "] " + "Heater is ready to drop")
         global UO
@@ -85,6 +86,8 @@ class mywindow(QMainWindow):
         icon_select = 1
 
     def cooler_main(self):
+        self.ui.pfdenv.mousePressEvent = self.pfd_mousepress
+        self.ui.pfdenv.mouseMoveEvent = self.pfd_mousemove
         self.ui.log_pan.append(
             "[" + str(time.strftime("%H:%M:%S", time.localtime())) + "] " + "Cooler is ready to drop")
         global UO
